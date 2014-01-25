5a03c7ef1d6f817e7f125d74cf72bac7e507d65e
Krzysztof Benedyczak <golbi@icm.edu.pl>
2014-01-25 21:18:43 +0000

    Added support classes for SAML bindings

---
+++ src/main/java/eu/unicore/samly2/binding/SAMLMessageType.java
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2014 ICM Uniwersytet Warszawski All rights reserved.
+ * See LICENCE.txt file for licensing information.
+ */
+package eu.unicore.samly2.binding;
+
+public enum SAMLMessageType {SAMLRequest, SAMLResponse}
---
+++ src/main/java/eu/unicore/samly2/binding/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2014 ICM Uniwersytet Warszawski All rights reserved.
+ * See LICENCE file for licencing information.
+ *
+ * Created on 25-01-2014
+ * Author: K. Benedyczak <golbi@mat.umk.pl>
+ */
+
+/**
+ * SAML bindings support
+ * @author K. Benedyczak
+ */
+package eu.unicore.samly2.binding;
